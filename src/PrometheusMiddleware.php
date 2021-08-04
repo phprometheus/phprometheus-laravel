f00f735b8e8d4e6e8d9f7d25f10ebdc64397d323
Kieran Patel <me@kieranajp.co.uk>
2021-08-04 13:21:29 +0200

    Add laravel route middleware

---
+++ src/PrometheusMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phprometheus\Laravel;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
+use Symfony\Component\HttpFoundation\Response;
+use Phprometheus\Prometheus;
+
+class PrometheusMiddleware
+{
+    private $routes;
+
+    private $prometheus;
+
+    public function __construct(Router $router, Prometheus $prometheus)
+    {
+        $this->routes = $router->getRoutes();
+        $this->prometheus = $prometheus;
+    }
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        $route = $this->routes->match($request);
+
+        $start = microtime(true);
+        /** @var Response $response */
+        $response = $next($request);
+        $duration = microtime(true) - $start;
+
+        $this->prometheus->observeHistogram(new RequestDuration([
+            'method' => $request->method(),
+            'route' => $route->uri(),
+            'status_code' => $response->getStatusCode(),
+        ]), $duration);
+
+        return $response;
+    }
+}
